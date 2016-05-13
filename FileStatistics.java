96d4c1876c65d63f8be9e48c0b922890f591399b
anavikajla <anavi1235@gmail.com>
2016-05-14 01:31:51 +0530

    updated with documentation

--- FileStatistics.java
+++ FileStatistics.java
@@ -1,3 +1,11 @@
+/** Go back to programming assignment 1 and optimize the performance of part (e) (only), using any techniques that might help.
+The program should take 1 argument which is a path to the top level directory, under which it will compute statistics for the different types of files.
+
+Author: Anavi Kajla
+Date: 13 May, 2016 
+
+*/
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -5,7 +13,7 @@ import java.util.*;
 public class FileStatistics{
 	
 	static String[] TYPES = new String[]{"jpg", "png", "gif", "mp4", "mp3", "exe", "psd", "html", "xml"};
-    static Map<String, Long> sizeOfFiles = new HashMap<>();
+    	static Map<String, Long> sizeOfFiles = new HashMap<>();
 	static Map<String, Long> numberOfFiles = new HashMap<>();
 	
 	public static void main(String args[])
@@ -15,17 +23,17 @@ public class FileStatistics{
 		File workingDirectory = new File (".");
 		
 		for(String extension:TYPES){
-			sizeOfFiles.put(extension,Long.valueOf(0));
-			numberOfFiles.put(extension, Long.valueOf(0));
+			sizeOfFiles.put(extension,Long.valueOf(0)); //initializes value of hashmap for size
+			numberOfFiles.put(extension, Long.valueOf(0)); //initializes value of hashmap for count
 		}
 		
-		printFileSizeAndCount(workingDirectory);
+		printFileSizeAndCount(workingDirectory); //call to method
 		
 		for(String type:sizeOfFiles.keySet())
 		{			
 			System.out.print(type);
-			String formatSize = String.format("%,d", (sizeOfFiles.get(type)));
-			System.out.println(numberOfFiles.get(type) + " " + formatSize);
+			String formattedSize = String.format("%,d", (sizeOfFiles.get(type))); //formats the number to display with commas every three characters from the right
+			System.out.println(numberOfFiles.get(type) + " " + formattedSize);
 		}
 		
 		long endTime = System.currentTimeMillis();
@@ -35,12 +43,11 @@ public class FileStatistics{
 	
 	/** e) Gives the total size of all the files represented by an extension */
 	
-	public static void printFileSizeAndCount(File f)
+	public static void printFileSizeAndCount(File filename)
 	{
-		long size=0;
-		File[] path = f.listFiles();
+		File[] listOfAllFiles = filename.listFiles();
 		
-			for(File file:path)
+			for(File file:listOfAllFiles)
 			{
 				if(file.isFile())
 				{
